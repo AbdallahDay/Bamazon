2e185bb848c77a24b49f2cba747296a97f937cee
Abdallah Day <10871403+AbdallahDay@users.noreply.github.com>
2018-09-22 02:51:59 -0500

    initial commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,145 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon_DB"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+
+    console.log("connected as id " + connection.threadId);
+    start();
+});
+
+function start() {
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", (err, res) => {
+        if (err) throw err;
+
+        console.log("");
+
+        const col1width = Math.max.apply(Math, res.map(function(o) { return o.item_id.toString().length; }));
+        const col2width = Math.max.apply(Math, res.map(function(o) { return o.product_name.length; }));
+        const col3width = Math.max.apply(Math, res.map(function(o) { return o.price.toString().length; }));
+        const col4width = Math.max.apply(Math, res.map(function(o) { return o.stock_quantity.toString().length; }));
+
+        const col1heading = "ID";
+        const col2heading = "Product Name";
+        const col3heading = "Price";
+        const col4heading = "QOH";
+
+        if (col1heading.length > col1width) col1width = col1heading.length;
+        if (col2heading.length > col2width) col2width = col2heading.length;
+        if (col3heading.length > col3width) col3width = col3heading.length;
+        if (col4heading.length > col4width) col4width = col4heading.length;
+
+        //print table headings
+        console.log([
+            col1heading + ((col1width > col1heading.length) ? " ".repeat(col1width - col1heading.length) : ""),
+            col2heading + ((col2width > col2heading.length) ? " ".repeat(col2width - col2heading.length) : ""),
+            col3heading + ((col3width > col3heading.length) ? " ".repeat(col3width - col3heading.length) : ""),
+            col4heading + ((col4width > col4heading.length) ? " ".repeat(col4width - col4heading.length) : "")
+        ].join(" "));
+
+        //print --- under headings
+        console.log([
+            "-".repeat(col1width),
+            "-".repeat(col2width),
+            "-".repeat(col3width),
+            "-".repeat(col4width)
+        ].join(" "));
+
+        //print rows
+        res.forEach(element => {
+            const col1spaces = col1width - element.item_id.toString().length;
+            const col2spaces = col2width - element.product_name.length;
+            const col3spaces = col3width - element.price.toString().length;
+            const col4spaces = col4width - element.stock_quantity.toString().length;
+
+            console.log([
+                element.item_id + " ".repeat(col1spaces),
+                element.product_name + " ".repeat(col2spaces),
+                element.price + " ".repeat(col3spaces),
+                element.stock_quantity + " ".repeat(col4spaces)
+            ].join(" "));
+        });
+
+        buyerPrompt();
+    });
+}
+
+function buyerPrompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the ID of the item you would like to purchase:"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like?"
+        }
+    ]).then(function(responses) {
+        const id = parseInt(responses.id);
+        const quantity = parseInt(responses.quantity);
+
+        connection.query(
+            "SELECT * FROM products WHERE ?",
+            {
+                item_id: id
+            },
+            function(err, res) {
+                if (err) throw err;
+
+                if (quantity > res[0].stock_quantity) {
+                    console.log("Insufficient quantity!");
+
+                    console.clear();
+                    start();
+                } else {
+                    purchaseItem(res[0], quantity);
+                }
+            }
+        );
+    })
+}
+
+function purchaseItem(item, quantityPurchased) {
+    // update database
+    updateRow(
+        item.item_id,
+        {
+            stock_quantity: item.stock_quantity - quantityPurchased
+        },
+        function(err, res) {
+            if (err) throw err;
+
+            // display total
+            if (res.affectedRows > 0) console.log(`Order complete! Your total: $${item.price * quantity}`);
+        }
+    );
+
+    buyerPrompt();
+}
+
+function updateRow(id, changeset, callback) {
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        changeset,
+        {
+            id: id
+        },
+        callback
+    );
+}
